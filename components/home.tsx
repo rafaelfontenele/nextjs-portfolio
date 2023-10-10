82cd2437fc2c73a2aa6102280cb806230d1258af
Rafael <98551688+etzoider@users.noreply.github.com>
2023-10-10 12:46:34 -0300

    feat: homepage text below image

--- components/home.tsx
+++ components/home.tsx
@@ -9,36 +9,41 @@ export default function Home() {
   return (
     <section id='#home'>
         
-        <motion.div className="flex items-center justify-center"
-        initial={ {opacity: 0, y: 0, x: 0} }
-        animate={ {opacity: 1, y: 0, x: 0} }
-        transition={{ delay: 1, duration: 2}}
-        >
-            <motion.div className='relative'
-            initial={ {rotate: 0, opacity: 1} }
-            animate={ {rotate: 0, opacity: 1} }
-            transition={ {delay: 1} }
-            >
-                <motion.div className='rounded-full border-black border-dotted border-4'
-                initial={{rotate: 0}}
-                animate={{rotate: 360}}
+        <div className="flex items-center justify-center">
+            <div className='relative'>
+                <motion.div className='w-[9rem] h-[9rem] rounded-full border-gray-900 border-dashed border-4 absolute'
+                initial={ {rotate: 0, x: 0, y: 0, opacity: 0.6, scale: 0.7} }
+                animate={ {rotate: 360, x: -2, y: -2, opacity: 0, scale: 1.05} }
+                transition={ {duration:5} }
+
                 >
-                    <motion.div>
+                    </motion.div>
+                    <motion.div
+                    initial={ { opacity: 0} }
+                    animate={ { opacity: 1, scale: 1} }
+                    transition={ {delay:3, duration: 3} }
+                    >
                         <Image 
                         src={randomGuyImage}
                         alt='Personal profile picture'
-                        className='w-24 h-24 rounded-full object-cover shadow-xl relative'
+                        className='w-[9rem] h-[9rem]  rounded-full border-2 border-gray-300 object-cover shadow-2xl relative'
                         width='192'
                         height='192'
                         quality='20'
                         priority={true}
                         />
                     </motion.div>
-                </motion.div>
-            </motion.div>
+            </div>
 
 
-        </motion.div>        
+        </div>        
+
+        <p className='flex flex-wrap justify-center items-center text-center mt-4 mb-10 px-4 flex-col text-2xl sm:text-l font-medium gap-2'>
+            <span className='font-bold'>Hello, I&apos;m <span className='font-bold'>Rafael</span>.</span> 
+            <span>I&apos;m a brazilian software developer<br></br> who enjoys building <span className='italic'>web apps.</span></span> 
+            <span>Currently, my focus is <span className='underline'>React (Next.js)</span></span>
+
+        </p>
 
     </section>
     )
